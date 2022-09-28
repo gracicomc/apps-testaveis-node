6764afc517003779bd80310a98b7becf26e7e2aa
Graciela Parente <graciparentte@gmail.com>
2022-09-28 17:56:09 -0300

    feat: create appointment usecase

---
+++ src/use-cases/create-appointment.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { Appointment } from "../entities/appointment";
+import { CreateAppointment } from "./create-appointment";
+
+describe('Create Appointment', () => {
+    it('should be able to create an appointment', () => {
+        const createAppointment = new CreateAppointment()
+
+        const startsAt = new Date()
+        const endsAt = new Date()
+    
+        endsAt.setDate(endsAt.getDate() + 1)
+
+        expect(createAppointment.execute({
+            customer: 'Jonh Doe',
+            startsAt,
+            endsAt
+        })).resolves.toBeInstanceOf(Appointment)
+    })
+})
